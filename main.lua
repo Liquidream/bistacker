e3997cf2fd55969c651402106c87736fe92119ee
Paul <liquidream@hotmail.co.uk>
2019-05-17 19:37:15 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,19 @@
+require 'bits'
+TS,N,x,y,p,m,w=0,9,5,N,3,.2,{}
+function _D()
+  BG(3)
+  for i=1,N do
+    if(w[i])then
+
+    end
+  end
+  H(y,x,x+(p-1),7)
+
+  --A(x,y)
+end
+function _U(dt)
+  FX(s*4)
+  x=x+m
+  if(x<2 or x>N-p+1)then m=m*-1 end
+  if(s==1)then w[y]=GR(y) y=y-1 end
+end
