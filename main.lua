17e73daa1b3850b8f5cc4b0968aca97b4ccff2f1
Paul <liquidream@hotmail.co.uk>
2019-05-17 19:45:06 +0100

    Line up

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ function _D()
   BG(3)
   for i=1,N do
     if(w[i])then
-
+      H(i,w[i][1],w[i][1]+(w[i][2]-1),7)
     end
   end
   H(y,x,x+(p-1),7)
@@ -15,5 +15,5 @@ function _U(dt)
   FX(s*4)
   x=x+m
   if(x<2 or x>N-p+1)then m=m*-1 end
-  if(s==1)then w[y]=GR(y) y=y-1 end
+  if(s==1)then w[y]={x,p} y=y-1 end
 end
